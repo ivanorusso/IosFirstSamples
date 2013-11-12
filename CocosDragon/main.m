dcd893c292759af92b34f73376ac0e1b25adeb19
Ivano Russo <home@iMac-di-Ivano.local>
2013-11-11 20:24:12 -0800

    Initial Commit

---
+++ CocosDragon/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  CocosDragon
+//
+//  Created by Ivano Russo on 11/11/13.
+//  Copyright Ivano Russo 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
